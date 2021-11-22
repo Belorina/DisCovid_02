7c0db275ed0d5930e26186337598a4ce862f294d
Belorina <ozcan_berrin@hotmail.com>
2021-11-22 18:58:26 +0100

    KillZone script

---
+++ Assets/#Project/Scripts/KillZone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    public Pool pool;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ClientBehaviour client = other.GetComponent<ClientBehaviour>();
+
+        if (client != null)
+        {
+            pool.Kill(client);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
